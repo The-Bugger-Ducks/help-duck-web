08931ff1d4082e52c8dff701083279eda12445d5
Giovana O. Silva <78885451+gioliveirass@users.noreply.github.com>
2022-03-31 13:21:57 -0300

    feat: creating login page structure

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,9 @@ import "../styles/components/Header.css";
 
 export default function Header() {
   return (
-    <nav className="header">
+    <header className="header">
       <h1>Help Duck</h1>
       <span>Olá, fulano</span>
-    </nav>
+    </header>
   );
 }
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,3 +1,5 @@
+import Button from "../../components/Button";
+import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import "../../styles/pages/Login/Login.css";
 
@@ -6,6 +8,38 @@ export default function Login() {
     <div id="login">
       <div className="login-container">
         <Header />
+        <div className="login-content">
+          <section className="login-welcome">
+            <h2>Bem vindo(a)!</h2>
+            <h1>Entre na sua conta</h1>
+          </section>
+          <form className="login-form">
+            <section className="login-data">
+              <label htmlFor="email">E-mail</label>
+              <input
+                type="email"
+                name="email"
+                className="email"
+                placeholder="jhon.snow@email.com"
+                required
+              />
+
+              <label htmlFor="password">Senha</label>
+              <input
+                type="password"
+                name="password"
+                className="password"
+                placeholder="senha"
+                required
+              />
+            </section>
+            <span id="recover-password">Esqueceu a senha?</span>
+            <Button type="submit" width="80%">
+              Entrar
+            </Button>
+          </form>
+        </div>
+        <Footer />
       </div>
     </div>
   );
