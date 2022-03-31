6d67cfbe406ef82df605444e721b0d00925faa52
Giovana O. Silva <78885451+gioliveirass@users.noreply.github.com>
2022-03-31 19:45:17 -0300

    Feat/creating login page (#24)
    * feat: creating header component
    
    * feat: creating login page structure
    
    * feat: saving form data and checking login status
    
    * feat: adding functionality to components
    
    * fix: componentizing form input

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,3 +1,69 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Button from "../../components/Button";
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import TextField from "../../components/TextField";
+import "../../styles/pages/Login/Login.css";
+
 export default function Login() {
-  return <p>Olá</p>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const isAuthenticated = false;
+
+  async function handleLogin(
+    emailParameter: string,
+    passwordParameter: string
+  ) {
+    const email = emailParameter;
+    const password = passwordParameter;
+  }
+
+  if (isAuthenticated) {
+    console.log("Authenticated");
+    navigate("/homepage");
+  }
+
+  return (
+    <div id="login">
+      <div className="login-container">
+        <Header />
+        <div className="login-content">
+          <section className="login-welcome">
+            <h2>Bem vindo(a)!</h2>
+            <h1>Entre na sua conta</h1>
+          </section>
+          <form className="login-form">
+            <section className="login-data">
+              <label htmlFor="email">E-mail</label>
+              <TextField
+                placeholder="jhon.snow@email.com"
+                onChange={(event) => setEmail(event.target.value)}
+                name="email"
+              />
+              <label htmlFor="password">Senha</label>
+              <TextField
+                placeholder="Senha"
+                onChange={(event) => setPassword(event.target.value)}
+                name="password"
+                type="password"
+              />
+            </section>
+            <Link to={"#"} id="recover-password">
+              Esqueceu a senha?
+            </Link>
+            <Button
+              type="submit"
+              width="80%"
+              onClick={() => handleLogin(email, password)}
+            >
+              Entrar
+            </Button>
+          </form>
+        </div>
+        <Footer />
+      </div>
+    </div>
+  );
 }
