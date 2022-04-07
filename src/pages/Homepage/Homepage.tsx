8e06c4ed15c93f635ff620fd80a9cfea333759b5
Giovana O. Silva <78885451+gioliveirass@users.noreply.github.com>
2022-04-07 08:32:40 -0300

    feat: adding search form and filter

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,9 +1,43 @@
+import Button from "../../shared/components/Button";
+import ChoiceField from "../../shared/components/ChoiceField";
+import Footer from "../../shared/components/Footer";
+import Header from "../../shared/components/Header";
+import TextField from "../../shared/components/TextField";
 import "../../shared/styles/pages/homepage/Homepage.css";
 
 export default function Homepage() {
   return (
     <div id="homepage">
-      <p>Olá</p>
+      <Header />
+      <div className="homepage-container">
+        <h1>Chamados</h1>
+        <section className="search-or-filter">
+          <form className="searchTicket">
+            <TextField
+              placeholder="Buscar por título do chamado"
+              name="search"
+              width="75%"
+            />
+            <Button width="20%" type="submit" fontSize="0.8rem">
+              Buscar
+            </Button>
+          </form>
+          <div className="filter">
+            <ChoiceField
+              name="filter"
+              items={[
+                {
+                  value: "opcao01",
+                  label: "Todos os chamados",
+                  selected: true,
+                },
+              ]}
+              width="100%"
+            />
+          </div>
+        </section>
+      </div>
+      <Footer />
     </div>
   );
 }
