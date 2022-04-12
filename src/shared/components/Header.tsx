bf6efb53f160f20dcdb23ad383edd254e83ccd66
Giovana O. Silva <78885451+gioliveirass@users.noreply.github.com>
2022-04-12 11:42:42 -0300

    feat: displaying username

--- src/shared/components/Header.tsx
+++ src/shared/components/Header.tsx
@@ -1,15 +1,21 @@
 import "../styles/components/Header.css";
 import Dropdown from "../../shared/components/Dropdown";
+import SessionController from "../utils/handlers/SessionController";
 
 interface PropsHeader {
   hiddenDropdown: boolean;
 }
 
 const Header: React.FC<PropsHeader> = ({ hiddenDropdown = false }) => {
+  const userInformation = SessionController.getUserInfo();
+
   return (
     <header className="header">
       <h1>Help Duck</h1>
-      <Dropdown name="Fulanoo" hiddenDropdown={hiddenDropdown} />
+      <Dropdown
+        name={`${userInformation?.firstName} ${userInformation?.lastName}`}
+        hiddenDropdown={hiddenDropdown}
+      />
     </header>
   );
 };
