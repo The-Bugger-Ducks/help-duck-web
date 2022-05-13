1665e9d3c8a9a05bfaedbb9d9513d6d90e6b201b
Giovana O. Silva <78885451+gioliveirass@users.noreply.github.com>
2022-05-13 10:44:02 -0300

    fix: remove console.log

--- src/pages/Ticket/DetailTicket.tsx
+++ src/pages/Ticket/DetailTicket.tsx
@@ -62,7 +62,6 @@ export default function DetailTicket() {
 
     const date = new Date(response.createdAt);
     setCreatedAt(date);
-    console.log(ticket);
   };
 
   const handleSubmitComment = async () => {
