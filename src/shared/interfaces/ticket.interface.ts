f7d2f89d05b0a33741a91e2462419a761e402247
thhenrique <henriquethiago298@gmail.com>
2022-05-15 02:32:12 -0300

    add solution comment view in details ticket

--- src/shared/interfaces/ticket.interface.ts
+++ src/shared/interfaces/ticket.interface.ts
@@ -1,18 +1,21 @@
 import { User } from "./user.interface"
 import Comment from "./comment.interface"
+import Solution from "./solution.interface";
 
 export default interface Ticket {
-  id: string
+  id: string;
   title: string;
   description: string;
   tags: Array<string>;
   priorityLevel: "high" | "medium" | "low";
-  status: "underAnalysis" | "awaiting" | "done",
-  reserved: boolean,
-  createdAt: Date,
-  updatedAt?: Date,
+  status: "underAnalysis" | "awaiting" | "done";
+  reserved: boolean;
+  createdAt: Date;
+  updatedAt?: Date;
 
-  user: User,
-  support: User,
-  comments: Array<Comment>
+  user: User;
+  support: User;
+  comments: Array<Comment>;
+
+  solution: Solution
 }
