13badfbc6bcf6b0d17bb7a33d075c8325c76a3a9
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2022-05-26 11:40:54 -0300

    :sparkles: feat: create solutions list component

---
+++ src/shared/components/SolutionsList.tsx
@@ -0,0 +1,134 @@
+import React, { useEffect, useState } from 'react';
+
+import SessionController from '../utils/handlers/SessionController';
+import { Problem, ProblemSolution } from '../interfaces/problem.interface';
+import { ProblemRequests } from '../utils/requests/Problem.requests';
+import CustomTableRow from './Loading/CustomTableRow';
+
+import '../styles/components/SolutionsList.css';
+import Button from './Button';
+
+interface SolutionsListProps {
+  problem?: Problem;
+  keyword?: string;
+}
+
+const SolutionsList: React.FC<SolutionsListProps> = ({ problem, keyword }) => {
+  const [loading, setLoading] = useState(false);
+  const [solutions, setSolutions] = useState<ProblemSolution[]>([]);
+  const [searchResult, setSearchResult] = useState([]);
+
+  const problemRequest = new ProblemRequests();
+  const userInformation = SessionController.getUserInfo();
+
+  useEffect(() => {
+    problem != null && getSolutionsByProblem(problem.id);
+    keyword != null && getSolutionsByKeyword(keyword);
+  }, []);
+
+  async function getSolutionsByProblem(id: string) {
+    setLoading(true);
+
+    const problem = await problemRequest.getProblem(id);
+    setSolutions(problem?.solutionList ?? []);
+
+    setLoading(false);
+  }
+
+  async function getSolutionsByKeyword(keyword: string) {
+    console.log('buscando!');
+  }
+
+  function getPossibleSolutionsList() {
+    return (
+      <section className="solution-list-container">
+        <h3>Soluções possíveis</h3>
+        <div className="grid-solutions">
+          <table>
+            <tbody>
+              <tr>
+                <th>Descrição da solução</th>
+              </tr>
+              {solutions.length > 0 ? (
+                solutions.map((solution, index) => {
+                  return (
+                    <>
+                      <tr
+                        key={index}
+                        onClick={() => console.log(solution.title)}
+                      >
+                        <td className="ticket-title">{solution.title}</td>
+                      </tr>
+                    </>
+                  );
+                })
+              ) : (
+                <CustomTableRow
+                  loading={loading}
+                  colSpan={4}
+                  typeTableRowText="soluções"
+                />
+              )}
+            </tbody>
+          </table>
+        </div>
+        <div className="new-solution">
+          <Button
+            color="#F2F2F3"
+            backgroundColor="#ADB5BD"
+            width="12rem"
+            height="2rem"
+            fontSize="0.8rem"
+            fontWeight="600"
+            onClick={() => console.log('nova solução')}
+          >
+            Nova solução
+          </Button>
+        </div>
+      </section>
+    );
+  }
+
+  function getSearchResult() {
+    return (
+      <section className="ticket-list-container">
+        <div className="grid-tickets">
+          <table>
+            <tbody>
+              <tr>
+                <th>Título do problema</th>
+                <th>Descrição da solução</th>
+              </tr>
+              {searchResult.length > 0 ? (
+                searchResult.map((result, index) => {
+                  return (
+                    <>
+                      <tr key={index} onClick={() => console.log(result)}>
+                        <td className="ticket-title">{result}</td>
+                        <td>{result}</td>
+                      </tr>
+                    </>
+                  );
+                })
+              ) : (
+                <CustomTableRow
+                  loading={loading}
+                  colSpan={4}
+                  typeTableRowText="busca"
+                />
+              )}
+            </tbody>
+          </table>
+        </div>
+      </section>
+    );
+  }
+
+  if (userInformation?.role === 'support') {
+    return getPossibleSolutionsList();
+  } else {
+    return getSearchResult();
+  }
+};
+
+export default SolutionsList;
